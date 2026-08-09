8ac62264d66c94676fa1faeed556619299f5bd8a
urakawakazuo0301 <urakawakazuo0301@gmail.com>
2026-08-10 07:16:56 +0900

    【追加】Bookmarkモデルとマイグレーション

---
+++ app/Models/Bookmark.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Bookmark extends Model
+{
+    protected $fillable = [
+        'title',
+        'url',
+        'description',
+        'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
